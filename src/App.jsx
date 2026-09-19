c12089aa85a533be4f8ce76859d313b6e44a9417
eduartolozao-web <eduar.tolozao@uniagustiniana.edu.co>
2026-09-19 02:43:13 +0000

    booleano

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import String from './string.jsx'
+import Booleano from './Booleano.jsx'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,7 +9,7 @@ function App() {
   return (
     <>
      
-       <String />
+       <Booleano />
       
     </>
   )
